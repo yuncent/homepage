5fa57ad8e69a0737736dcc7185563c18a9b4510f
yuncent <63391529+yuncent@users.noreply.github.com>
2025-01-31 23:03:37 +0800

    Update video.js

--- js/video.js
+++ js/video.js
@@ -8,6 +8,6 @@
 
 document.writeln("<table align=\'center\' border=\'0\' width=\'1024px\' >");
 document.writeln("  <tr>");
-document.writeln("  <td align=\'center\'><a href=\'https://sxso.cn/\'><video src=\'im/helloworld.mp4\' width=\'480\' height=\'300\' align=\'middle\' title=\'欢迎进入云彩天际网站\' autoplay muted loop ></video></a></td>");
+document.writeln("  <td align=\'center\'><a href=\'https://sxso.cn/\'><video src=\'im/helloworld.mp4\' style=\'mix-blend-mode: screen; display: block\' width=\'480\' height=\'309\' align=\'middle\' title=\'欢迎进入云彩天际网站\' autoplay muted loop ></video></a></td>");
 document.writeln("  </tr> ");
 document.writeln("</table>");
