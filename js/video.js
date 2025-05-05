d60e916248c5bb94565f3da49919eb7377df63b1
yuncent <63391529+yuncent@users.noreply.github.com>
2025-05-05 15:32:52 +0800

    Update video.js

--- js/video.js
+++ js/video.js
@@ -47,7 +47,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
   // 创建超链接
   const link = document.createElement('a');
-  link.href = 'https://www.sxso.cn/';
+  link.href = 'https://www.sosxs.com/';
 
   // 创建视频元素
   const video = document.createElement('video');
